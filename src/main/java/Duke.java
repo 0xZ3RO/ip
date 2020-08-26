0c1be35aa557a14dd27d5dbe1da6cd698d504afa
0xZ3RO <>
2020-08-26 19:52:31 +0900

    Add A-CodingStandard: Follow the Coding Standard

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,26 +10,27 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println(hLine + " Hello! I'm Duke\n What can I do for you?\n" + hLine);
         String echo = "";
-        Task[] list = new Task[100];
+        Task[] tasks = new Task[100];
         int listSize = 0;
         Scanner sc = new Scanner(System.in);
-        while (!echo.equals("bye")){
+        while (!echo.equals("bye")) {
             echo = sc.nextLine();
-            if (echo.equals("list")){
-                System.out.println(hLine + " Here are the tasks in your list:");
-                for (int i = 0; i < listSize; i++){
-                    System.out.println(" " + (i+1) + ".["+ list[i].getStatusIcon() + "] " + list[i].description);
+            if (echo.equals("list")) {
+                System.out.println(hLine + " Here are the tasks in your tasks:");
+                for (int i = 0; i < listSize; i++) {
+                    System.out.println(" " + (i + 1) + ".[" + tasks[i].getStatusIcon() + "] "
+                            + tasks[i].description);
                 }
                 System.out.println(hLine);
-            } else if (echo.length() > 5 && echo.substring(0,4).equals("done")) {
-                list[Integer.valueOf(echo.substring(5))-1].setDone();
+            } else if (echo.length() > 5 && echo.substring(0, 4).equals("done")) {
+                tasks[Integer.valueOf(echo.substring(5)) - 1].markAsDone();
                 System.out.println(hLine + " Nice! I've marked this task as done: ");
-                System.out.println("   [\u2713] " + list[Integer.valueOf(echo.substring(5))-1].description
+                System.out.println("   [\u2713] " + tasks[Integer.valueOf(echo.substring(5)) - 1].description
                         + "\n" + hLine);
-            } else if (!echo.equals("bye")){
-                    list[listSize] = new Task(echo);
-                    listSize++;
-                    System.out.println(hLine + " added: " + echo + "\n" + hLine);
+            } else if (!echo.equals("bye")) {
+                tasks[listSize] = new Task(echo);
+                listSize++;
+                System.out.println(hLine + " added: " + echo + "\n" + hLine);
             }
         }
         sc.close();
