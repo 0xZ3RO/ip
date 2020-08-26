79caa92179367511fcec15c448da2714989189c6
0xZ3RO <>
2020-08-26 14:54:29 +0900

    Implement Level 2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,10 +10,22 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println(hLine + " Hello! I'm Duke\n What can I do for you?\n" + hLine);
         String echo = "";
+        String[] list = new String[100];
+        int listSize = 0;
         Scanner sc = new Scanner(System.in);
         while (!echo.equals("bye")){
             echo = sc.nextLine();
-            System.out.println(hLine + " " + echo + "\n" + hLine);
+            if (echo.equals("list")){
+                System.out.print(hLine);
+                for (int i = 0; i < listSize; i++){
+                    System.out.println(" " + (i+1) + ". " + list[i]);
+                }
+                System.out.println(hLine);
+            }else if (!echo.equals("bye")){
+                list[listSize] = echo;
+                listSize++;
+                System.out.println(hLine + " added: " + echo + "\n" + hLine);
+            }
         }
         sc.close();
         System.out.println(hLine + " Bye! Hope to see you again soon!\n" + hLine);
