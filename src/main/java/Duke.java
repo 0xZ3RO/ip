fd58d6e88673af34eec7e2dcf447c9d83c749b0c
0xZ3RO <>
2020-08-31 12:39:04 +0900

    Add Level-4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,7 +8,7 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println(hLine + " Hello! I'm Duke\n What can I do for you?\n" + hLine);
+        System.out.println(hLine + logo + " Hello! I'm Duke\n What can I do for you?\n" + hLine);
         String echo = "";
         Task[] tasks = new Task[100];
         int listSize = 0;
@@ -18,19 +18,35 @@ public class Duke {
             if (echo.equals("list")) {
                 System.out.println(hLine + " Here are the tasks in your tasks:");
                 for (int i = 0; i < listSize; i++) {
-                    System.out.println(" " + (i + 1) + ".[" + tasks[i].getStatusIcon() + "] "
-                            + tasks[i].description);
+                    System.out.println(" " + (i + 1) + "." + tasks[i]);
                 }
                 System.out.println(hLine);
             } else if (echo.length() > 5 && echo.substring(0, 4).equals("done")) {
                 tasks[Integer.valueOf(echo.substring(5)) - 1].markAsDone();
                 System.out.println(hLine + " Nice! I've marked this task as done: ");
-                System.out.println("   [\u2713] " + tasks[Integer.valueOf(echo.substring(5)) - 1].description
-                        + "\n" + hLine);
+                System.out.println("   " + tasks[Integer.valueOf(echo.substring(5)) - 1] + "\n" + hLine);
             } else if (!echo.equals("bye")) {
-                tasks[listSize] = new Task(echo);
+                String tmp = echo.trim().split("\\s+")[0];
+                System.out.println(hLine + " Got it. I've added this task:");
+                if (tmp.equals("todo")) {
+                    tasks[listSize] = new ToDos(echo.substring(5));
+                    System.out.println("   " + tasks[listSize]);
+                } else if (tmp.equals("deadline")) {
+                    tmp = echo.trim().substring(9);
+                    String[] tmp2 = tmp.split(" /by ");
+                    tasks[listSize] = new Deadlines(tmp2[0], tmp2[1]);
+                    System.out.println("   " + tasks[listSize]);
+                } else if (tmp.equals("event")) {
+                    tmp = echo.trim().substring(6);
+                    String[] tmp2 = tmp.split(" /at ");
+                    tasks[listSize] = new Events(tmp2[0], tmp2[1]);
+                    System.out.println("   " + tasks[listSize]);
+                } else {
+                    tasks[listSize] = new Task(echo);
+                    System.out.println("   " + echo);
+                }
                 listSize++;
-                System.out.println(hLine + " added: " + echo + "\n" + hLine);
+                System.out.println(" Now you have " + listSize + " tasks in the list.\n" + hLine);
             }
         }
         sc.close();
